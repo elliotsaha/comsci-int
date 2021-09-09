0120db3506286ae65329894abd57e1c2f9f6ae47
HerilS <herilsaha06@gmail.com>
2021-09-09 12:17:20 -0600

    Refactored Functions Assignment

--- src/FunctionsAssignment/js/Main/main.js
+++ src/FunctionsAssignment/js/Main/main.js
@@ -2,33 +2,46 @@
 var luminanceAnswer = document.getElementById("luminanceAnswer");
 var rgbTextInput = document.getElementById("rgbTextInput");
 var rgbButtonInput = document.getElementById("rgbButtonInput");
-var getLuminance = function () {
+// converts rgb string into number array
+var rgbStringParser = function (rgbString, eightBit) {
     // split passed rgb string into array (e.g. rgb(nnn, nnn, nnn) -> ["nnn", "nnn", "nnn"])
     var marker = "(";
-    var numArr = rgbTextInput.value
+    var numArr = rgbString.value
         .split(marker)
         .pop()
         .slice(0, -1)
         .split(",");
-    var Luminance = 0;
+    for (var i = 0; i < numArr.length; i++) {
+        numArr[i] = parseFloat(numArr[i]);
+        if (eightBit) {
+            numArr[i] /= 255;
+        }
+    }
+    return numArr;
+};
+var applyChannelFormula = function (rgb8BitArray) {
     var RGBcoeffiecients = [0.2126, 0.7152, 0.0722];
     // loop through input rgb string array
-    for (var i = 0; i < numArr.length; i++) {
-        // turn each string in array into number
-        numArr[i] = parseFloat(numArr[i]);
-        // turn 16bit rgb into 8bit
-        numArr[i] /= 255;
+    for (var i = 0; i < rgb8BitArray.length; i++) {
         // apply formula
-        if (numArr[i] <= 0.04045) {
-            numArr[i] /= 12.92;
+        if (rgb8BitArray[i] <= 0.04045) {
+            rgb8BitArray[i] /= 12.92;
         }
         else {
-            numArr[i] = Math.pow(((numArr[i] + 0.055) / 1.055), 2.4);
+            rgb8BitArray[i] = Math.pow(((rgb8BitArray[i] + 0.055) / 1.055), 2.4);
         }
         // each channel gets multiplied by their corrosponding coefficient
-        numArr[i] *= RGBcoeffiecients[i];
-        // add each 8-bit channel to Luminance
-        Luminance += numArr[i];
+        rgb8BitArray[i] *= RGBcoeffiecients[i];
+    }
+    return rgb8BitArray;
+};
+var getLuminance = function () {
+    var numArr = rgbStringParser(rgbTextInput, true);
+    var channelManipulatedArr = applyChannelFormula(numArr);
+    // add up channels to single luminance value
+    var Luminance = 0;
+    for (var i = 0; i < channelManipulatedArr.length; i++) {
+        Luminance += channelManipulatedArr[i];
     }
     // return rounded answer
     luminanceAnswer.innerHTML = "Luminance (Rounded to four decimal places): " + Luminance.toFixed(4);
--- src/FunctionsAssignment/js/Main/main.ts
+++ src/FunctionsAssignment/js/Main/main.ts
@@ -4,39 +4,53 @@ const rgbTextInput = document.getElementById(
 ) as HTMLInputElement;
 const rgbButtonInput = document.getElementById("rgbButtonInput");
 
-const getLuminance = () => {
+// converts rgb string into number array
+const rgbStringParser = (
+  rgbString: HTMLInputElement,
+  eightBit: boolean
+): Array<string | number> => {
   // split passed rgb string into array (e.g. rgb(nnn, nnn, nnn) -> ["nnn", "nnn", "nnn"])
   const marker = "(";
-  const numArr: Array<string | number> = rgbTextInput.value
+  const numArr: Array<string | number> = rgbString.value
     .split(marker)
     .pop()
     .slice(0, -1)
     .split(",");
-
-  let Luminance = 0;
-
-  const RGBcoeffiecients = [0.2126, 0.7152, 0.0722];
-
-  // loop through input rgb string array
   for (let i = 0; i < numArr.length; i++) {
-    // turn each string in array into number
     numArr[i] = parseFloat(numArr[i] as string);
+    if (eightBit) {
+      (numArr[i] as number) /= 255;
+    }
+  }
 
-    // turn 16bit rgb into 8bit
-    (numArr[i] as number) /= 255;
+  return numArr;
+};
 
+const applyChannelFormula = (rgb8BitArray: Array<number>): Array<number> => {
+  const RGBcoeffiecients = [0.2126, 0.7152, 0.0722];
+  // loop through input rgb string array
+  for (let i = 0; i < rgb8BitArray.length; i++) {
     // apply formula
-    if (numArr[i] <= 0.04045) {
-      (numArr[i] as number) /= 12.92;
+    if (rgb8BitArray[i] <= 0.04045) {
+      (rgb8BitArray[i] as number) /= 12.92;
     } else {
-      numArr[i] = (((numArr[i] as number) + 0.055) / 1.055) ** 2.4;
+      rgb8BitArray[i] = (((rgb8BitArray[i] as number) + 0.055) / 1.055) ** 2.4;
     }
 
     // each channel gets multiplied by their corrosponding coefficient
-    (numArr[i] as number) *= RGBcoeffiecients[i];
+    (rgb8BitArray[i] as number) *= RGBcoeffiecients[i];
+  }
+  return rgb8BitArray;
+};
 
-    // add each 8-bit channel to Luminance
-    Luminance += numArr[i] as number;
+const getLuminance = () => {
+  const numArr = rgbStringParser(rgbTextInput, true);
+  const channelManipulatedArr = applyChannelFormula(numArr as Array<number>);
+
+  // add up channels to single luminance value
+  let Luminance = 0;
+  for (let i = 0; i < channelManipulatedArr.length; i++) {
+    Luminance += channelManipulatedArr[i];
   }
 
   // return rounded answer
